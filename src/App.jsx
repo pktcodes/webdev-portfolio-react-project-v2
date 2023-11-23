586af6c72fd26c95ddabf390695095fd5a861eed
Praveen Kumar <praveenkumarthipparam@gmail.com>
2023-11-23 02:55:48 -0600

    Additional Setup & Assets

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 const App = () => {
-  return <h2 className="text-3xl font-bold underline">Hello World</h2>;
+  return (
+    <h2 className="container p-6 text-3xl font-bold underline">Hello World</h2>
+  );
 };
 
 export default App;
